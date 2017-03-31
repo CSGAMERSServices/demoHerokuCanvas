3c240b1b42f1a0a06c5e2f850831ef274a6644e6
Paul Roth <proth@salesforce.com>
2017-03-31 17:06:23 -0500

    UPDATE: cleanup config.

--- config/default.js
+++ config/default.js
@@ -1,3 +1,22 @@
+
+
 module.exports = {
-	"env": "default"
+	
+	//-- which environment
+	"env": "default",
+	
+	//-- defaults
+	"default": {
+		"PORT": 5000
+	},
+	
+	"statusCodes": {
+		"unauthorized": 401,
+		"unauthorizedText": "Unauthorized"
+	}
 };
+
+//-- post processing
+module.exports.default.STATUS_CODE = module.exports.statusCodes.unauthorized;
+module.exports.default.STATUS_TEXT = module.exports.statusCodes.unauthorizedText;
+
